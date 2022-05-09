a001bd72bcffd770191c81e20796d24fa61b7426
Prathibha reddy maram <97885447+Prathibha28@users.noreply.github.com>
2022-05-09 17:45:35 +0530

    added files to problem 3

---
+++ AI/task-1/problem 3/task-1 of AI pro 3.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+file = open("about.txt", "r")
+data = file.read()
+words = data.split(" ")
+# # Gets each line till end of file is reached
+# for line in file:
+#     # Splits each line into words
+#     words = line.split(" ")
+#     # Counts each word
+count = len(words)
+counting = Counter(words)
+for i in range(0,len(words)):
+    if len(words[i]) >= 6:
+        print(words[i])
+
+print("Number of words present in given file: " + str(count))
+
+list_of_word_and_frequency = counting.most_common()
+print("Most frequently used word in Text File is ", counting.most_common()[0][0])
+file.close()
